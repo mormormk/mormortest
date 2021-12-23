cc5288c31d3404ba736a59b075cda4339583d89c
mormormk <arbre.mk@gmail.com>
2021-12-23 23:23:01 +0900

    no message

--- js/common.js
+++ js/common.js
@@ -21,7 +21,17 @@ $(function () {
   });
 });
 
-// $(".mv-list").slick({
-//   centerMode: true,
-//   centerPadding: "25%",
-// });
+// TOPへ戻るボタンスクロールで現れる
+$(function () {
+  // 変数にクラスを入れる
+  var btn = $(".topback-btn");
+
+  //スクロールしてページトップから100に達したらボタンを表示
+  $(window).on("load scroll", function () {
+    if ($(this).scrollTop() > 300) {
+      btn.addClass("isActive");
+    } else {
+      btn.removeClass("isActive");
+    }
+  });
+});
